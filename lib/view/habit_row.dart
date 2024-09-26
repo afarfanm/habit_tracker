020e9d8f798fe6d81c1fcff40451f527c6cd4073
Andres Farfan <farfanm.andres@gmail.com>
2024-09-26 12:24:36 -0600

    Added style themes

--- lib/view/habit_row.dart
+++ lib/view/habit_row.dart
@@ -3,6 +3,7 @@ import 'package:habit_tracker/model/habit.dart';
 import 'package:habit_tracker/model/habits_dao.dart';
 import 'package:habit_tracker/view/habit_config_dialog.dart';
 import 'package:habit_tracker/view/habit_row_cell.dart';
+import 'package:habit_tracker/view/streak_counter.dart';
 
 class HabitRow extends StatefulWidget {
   const HabitRow(
@@ -22,98 +23,100 @@ class HabitRow extends StatefulWidget {
 class _HabitRowState extends State<HabitRow> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 2,
-          child: HabitRowCell(
-            child: IconButton(
-              onPressed: () => widget.onHabitDeletionRequested(widget.index),
-              icon: const Icon(Icons.delete),
-            ),
-          ),
-        ),
-        ...List.generate(
-          7,
-          (i) {
-            return Expanded(
-              flex: 3,
-              child: HabitRowCell(
-                child: Builder(builder: (context) {
-                  if (widget.habit.isMarkedNDaysAgo(7 - i)) {
-                    return const Icon(
-                      Icons.check,
-                      size: 40.0,
-                    );
-                  } else {
-                    return Container();
-                  }
-                }),
-              ),
-            );
-          },
-        ),
-        Expanded(
-          flex: 3,
-          child: HabitRowCell(
-            child: Transform.scale(
-              scale: 1.8,
-              child: Checkbox(
-                value: widget.habit.isMarkedToday(),
-                onChanged: (value) => toggleHabitMarkedToday(),
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      color: widget.index % 2 == 0
+          ? colorScheme.surfaceContainer
+          : colorScheme.surfaceContainerHigh,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: HabitRowCell(
+              child: IconButton(
+                onPressed: () => widget.onHabitDeletionRequested(widget.index),
+                icon: Icon(
+                  Icons.delete,
+                  color: colorScheme.secondary,
+                ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 2,
-          child: HabitRowCell(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  Icons.local_fire_department,
-                  size: 50.0,
-                  color: Colors.black
-                      .withOpacity(widget.habit.isMarkedToday() ? 1.0 : 0.2),
+          ...List.generate(
+            7,
+            (i) {
+              return Expanded(
+                flex: 3,
+                child: HabitRowCell(
+                  child: Builder(builder: (context) {
+                    if (widget.habit.isMarkedNDaysAgo(7 - i)) {
+                      return Icon(
+                        Icons.check,
+                        color: colorScheme.tertiary,
+                        size: 40.0,
+                      );
+                    } else {
+                      return Container();
+                    }
+                  }),
                 ),
-                Text(
-                  "${widget.habit.streak}",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black
-                        .withOpacity(widget.habit.isMarkedToday() ? 1.0 : 0.2),
-                  ),
+              );
+            },
+          ),
+          Expanded(
+            flex: 3,
+            child: HabitRowCell(
+              child: Transform.scale(
+                scale: 1.8,
+                child: Checkbox(
+                  value: widget.habit.isMarkedToday(),
+                  onChanged: (value) => toggleHabitMarkedToday(),
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 8,
-          child: HabitRowCell(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
-                  onPressed: showHabitEditDialog,
-                  icon: const Icon(Icons.edit),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      widget.habit.name,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
+          Expanded(
+            flex: 2,
+            child: HabitRowCell(
+              child: StreakCounter(
+                streakActive: widget.habit.isMarkedToday(),
+                streakCount: widget.habit.streak,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: HabitRowCell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: showHabitEditDialog,
+                    icon: Icon(
+                      Icons.edit,
+                      color: colorScheme.secondary,
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        widget.habit.name,
+                        style: textTheme.bodyLarge!
+                            .copyWith(color: colorScheme.tertiary),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
